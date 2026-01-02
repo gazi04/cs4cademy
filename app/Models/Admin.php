6215946531f555a5647731f8e639a2b07f813de1
gazi04 <gazmendhalili2016@gmail.com>
2026-01-02 18:19:56 +0100

    :card_file_box: Add migration to create an admin table, and add eloquent model

---
+++ app/Models/Admin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+final class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'admins';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return ['password' => 'hashed'];
+    }
+}
